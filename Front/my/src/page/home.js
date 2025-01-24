9b285f5a079d79df970c8d221d1458386d6bd179
hrutik2 <hrutik0729@gmail.com>
2025-01-24 08:55:38 +0530

    done

--- Front/my/src/page/home.js
+++ Front/my/src/page/home.js
@@ -1,85 +1,157 @@
-import styled from "styled-components"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { GetProductAction } from "../redux/action"
-import { useNavigate } from "react-router-dom"
-export const Home=()=>{
-    const navigate=useNavigate()
-    const dispatch=useDispatch()
-    const {cars}=useSelector(state=>state)
-    console.log(cars)
-    useEffect(()=>{
-        let token=localStorage.getItem("Authtoken")
-        if(token){
-            dispatch(GetProductAction())
-        }
-        else{
-            navigate("/Auth")
-        }
-    },[])
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GetProductAction } from "../redux/action";
+import { useNavigate } from "react-router-dom";
+
+export const Home = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { cars } = useSelector((state) => state);
+  const [sortByPrice, setSortByPrice] = useState("asc");
+  const [sortByMileage, setSortByMileage] = useState("asc");
+  const [color, setColor] = useState("");
+  const [data, setData] = useState([]);
+  const [Transmission, setTransmission] = useState("");
+   
+
+
+
+  useEffect(() => {
+    let token = localStorage.getItem("Authtoken");
+    if (token) {
+      dispatch(GetProductAction());
+    } else {
+      navigate("/Auth");
+    }
+  }, [dispatch, navigate]);
+
+  useEffect(() => {
+    let filteredData = [...cars];
+
     
-    return(
-        <Container>
-            <Sidebar>
-            <FilterContainer>
-                <FilterSection>
-                    <FilterTitle>Price Range</FilterTitle>
-                    <FilterInput 
-                        type="range" 
-                        min="0" 
-                        max="2000000" 
-                        step="10000"
-                    />
-                </FilterSection>
+    if (sortByPrice === "asc" && sortByPrice !== "") {
+      filteredData.sort((a, b) => { return a.price - b.price});
+    } else if (sortByPrice === "desc" && sortByPrice !== "") {
+      filteredData.sort((a, b) => { return b.price - a.price});
+    }
 
-                <FilterSection>
-                    <FilterTitle>Mileage</FilterTitle>
-                    <Select>
-                        <option value="">Select Mileage Range</option>
-                        <option value="0-10000">0 - 10,000 km</option>
-                        <option value="10000-30000">10,000 - 30,000 km</option>
-                        <option value="30000-50000">30,000 - 50,000 km</option>
-                        <option value="50000+">50,000+ km</option>
-                    </Select>
-                </FilterSection>
+   
+    if (sortByMileage === "asc" && sortByMileage !== "") {
+      filteredData.sort((a, b) => a.Mileage - b.Mileage);
+    } else if (sortByMileage === "desc" && sortByMileage !== "") {
+      filteredData.sort((a, b) => b.Mileage - a.Mileage);
+    }
 
-                <FilterSection>
-                    <FilterTitle>Color</FilterTitle>
-                    <ColorContainer>
-                        <ColorOption color="#000000" />
-                        <ColorOption color="#FF0000" />
-                        <ColorOption color="#FFFFFF" />
-                        <ColorOption color="#0000FF" />
-                        <ColorOption color="#808080" />
-                    </ColorContainer>
-                </FilterSection>
-            </FilterContainer>
-        </Sidebar>
-        <SubContainer>
-                {cars.map((item)=>(
-                  <div key={item._id}>
-                            <img src={item.image} alt={item.name} />
-                            <h2><i>{item.name}</i></h2>
-                            <p><b>Price: </b>{item.price}</p>
-                            <p><b>Description: </b>{item.description}</p>
-                            
-                            <p><b>Color: </b>{item.color}</p>
-                            <p><b>Mileage: </b>{item.Mileage}</p>
-                            <p><b>Year Of Manufatcher: </b>{item.yearOfManufatcher}</p>
-                            <p><b>Model: </b>{item.model}</p>
-                            <p><b>Transmission: </b>{item.transmission}</p>
-                            
-                        </div>
-                    )
-                )}
-                
-               
-            </SubContainer>
+   
+    if (color !== "" && color !== "Select Color") {
+      filteredData = filteredData.filter((item) => item.color.toLowerCase() === color.toLowerCase());
+    }
 
+    if (Transmission !== "" && Transmission !== "Select Transmission") {
+      filteredData = filteredData.filter((item) => item.transmission.toLowerCase() === Transmission.toLowerCase());
+    }
 
-        </Container>
-    )
-}
+    setData(filteredData);
+  }, [cars, sortByPrice, sortByMileage, color,Transmission]);
+  const clearFilters = () => {
+    setData([...cars]);
+    setSortByPrice("");
+    setSortByMileage("");
+    setColor("");
+    setTransmission("");
+  }
+  return (
+    <Container>
+      <Sidebar>
+        <FilterContainer>
+          <FilterSection>
+            <FilterTitle>Sort By Price</FilterTitle>
+            <Select onChange={(e) => setSortByPrice(e.target.value)}>
+                <option value="">Select Price</option>
+              <option value="asc">Low to High</option>
+              <option value="desc">High to Low</option>
+            </Select>
+          </FilterSection>
+
+          <FilterSection>
+            <FilterTitle>Sort By Mileage</FilterTitle>
+            <Select onChange={(e) => setSortByMileage(e.target.value)}>
+                <option value="">Select Mileage</option>
+              <option value="asc">Low to High</option>
+              <option value="desc">High to Low</option>
+            </Select>
+          </FilterSection>
+        
+          <FilterSection>
+            <FilterTitle>Select by Transmission</FilterTitle>
+            <Select onChange={(e) => setTransmission(e.target.value)}>
+              <option value="">Select Transmission</option>
+              <option value="Manual">Manual</option>
+              <option value="Automatic">Automatic</option>
+              <option value="CVT">CVT</option>
+              <option value="DCT">DCT</option>
+              <option value="AMT">AMT</option>
+              <option value="Other">Other</option>
+            </Select>
+          </FilterSection>
+          <FilterSection>
+            <FilterTitle>Color</FilterTitle>
+            <ColorContainer>
+                <option value="" onClick={() => setColor("")}>Select Color</option>
+              <ColorOption onClick={() => setColor("black")} color="black" />
+              <ColorOption onClick={() => setColor("silver")} color="silver" />
+              <ColorOption onClick={() => setColor("white")} color="white" />
+              <ColorOption onClick={() => setColor("blue")} color="blue" />
+              <ColorOption onClick={() => setColor("red")} color="red" />
+            </ColorContainer>
+          </FilterSection>
+        </FilterContainer>
+        <Button onClick={clearFilters}>Clear Filters</Button>
+      </Sidebar>
+
+      <SubContainer>
+        {data.map((item) => (
+          <div key={item._id}>
+            <img src={item.image} alt={item.name} />
+            <h2>
+              <i>{item.name}</i>
+            </h2>
+            <p>
+              <b>Price: </b>
+              {item.price}
+            </p>
+            <p>
+              <b>Description: </b>
+              {item.description}
+            </p>
+            <p>
+              <b>Color: </b>
+              {item.color}
+            </p>
+            <p>
+              <b>Mileage: </b>
+              {item.Mileage}
+            </p>
+            <p>
+              <b>Year Of Manufacturer: </b>
+              {item.yearOfManufacturer || "N/A"}
+            </p>
+            <p>
+              <b>Model: </b>
+              {item.model}
+            </p>
+            <p>
+              <b>Transmission: </b>
+              {item.transmission}
+            </p>
+          </div>
+        ))}
+      </SubContainer>
+    </Container>
+  );
+};
+
 const Container=styled.div`
  width:100%;
  display:flex;
@@ -150,6 +222,16 @@ transition: transform 0.2s;
 &:hover {
     transform: scale(1.1);
 }
+`;
+const Button = styled.button`
+    width: 50%;
+    margin:auto;
+    color:white;
+    background-color:blue;
+    padding: 8px;
+    border-radius: 4px;
+    border: 1px solid #ddd;
+    cursor: pointer;
 `;
 const SubContainer = styled.div`
      width:80%;
